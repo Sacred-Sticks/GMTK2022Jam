7e3e1c16a183e2709336bcfb0c4a2e7753ccecfc
Lucas Ackman <75407333+Sacred-Sticks@users.noreply.github.com>
2022-07-15 14:42:07 -0500

    Finished Jumping for Movement

--- GMTK Game Jam 2022/Assets/Scripts/Movement/PlayerMovement.cs
+++ GMTK Game Jam 2022/Assets/Scripts/Movement/PlayerMovement.cs
@@ -8,7 +8,11 @@ public class PlayerMovement : MonoBehaviour
     [Space]
     [SerializeField] private float movementSpeed;
     [SerializeField] private float jumpHeight;
-    [SerializeField] private float jumpTime;
+    [SerializeField] private float jumpWaitTime;
+    [Space]
+    [SerializeField] private Transform feet;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float groundDistance;
 
     private PlayerInputs inputs;
     private Rigidbody body;
@@ -16,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 movementInput;
     private Vector2 movement;
 
-    [SerializeField] bool jumping = false;
+    [SerializeField] bool isJumping = false;
 
     private void Awake()
     {
@@ -35,21 +39,37 @@ public class PlayerMovement : MonoBehaviour
     {
         movement = (transform.right * movementInput.x * movementSpeed) + transform.up * body.velocity.y;
         body.velocity = movement;
-        CalculateJump();
+        if (!(movementInput.y > 0)) return;
+        if (CheckGround() && !isJumping) StartCoroutine("Jump");
+    }
+
+    private bool CheckGround()
+    {
+        return Physics.CheckSphere(feet.position, groundDistance, groundLayer);
     }
 
     private void CalculateJump()
     {
         if (!(movementInput.y > 0)) return;
 
-        float jumpForce = Mathf.Sqrt(2 * jumpHeight * -Physics.gravity.y)/jumpTime;
-
-        if (!jumping)
+        if (!isJumping)
         {
-            float jumpVelocity = jumpHeight * 2 / jumpTime;
-            body.velocity = body.velocity.x * transform.right + jumpVelocity * transform.up;
-            jumping = true;
+            float jumpVelocity = Mathf.Sqrt(2 * jumpHeight * -Physics.gravity.y);
+            Vector3 jumpVelocityV3 = body.velocity.x * transform.right + jumpVelocity * transform.up;
+            Debug.Log(jumpVelocity);
+            body.velocity = jumpVelocityV3;
+            isJumping = true;
             Debug.Log("Jumping with velocity " + body.velocity.y);
         }
+    }
+
+    private IEnumerator Jump()
+    {
+        isJumping = true;
+        float jumpSpeed = Mathf.Sqrt(2 * jumpHeight * -Physics.gravity.y);
+        Vector3 jumpVelocityV3 = body.velocity.x * transform.right + jumpSpeed * transform.up;
+        body.velocity = jumpVelocityV3;
+        yield return new WaitForSeconds(jumpWaitTime);
+        isJumping = false;
     }
 }
