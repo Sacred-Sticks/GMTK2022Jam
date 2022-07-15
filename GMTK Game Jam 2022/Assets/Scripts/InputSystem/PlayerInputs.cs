3df6dca4092fa7c8d43d3618c8bfa3cab588f852
Lucas Ackman <75407333+Sacred-Sticks@users.noreply.github.com>
2022-07-15 13:15:39 -0500

    Added Basic Inputs

--- GMTK Game Jam 2022/Assets/Scripts/InputSystem/PlayerInputs.cs
+++ GMTK Game Jam 2022/Assets/Scripts/InputSystem/PlayerInputs.cs
@@ -1,18 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerInputs : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private InputActionAsset playerInputs;
+    [Space]
+    [SerializeField] private string actionMapStr;
+    [SerializeField] private string movementActionStr;
+    [SerializeField] private string firingActionStr;
+    [SerializeField] private string aimingActionStr;
+
+    private InputAction movementAction;
+    private InputAction firingAction;
+    private InputAction aimingAction;
+
+    private Vector2 movementValue;
+    private bool firingValue;
+    private Vector2 aimingValue;
+
+    private void Awake()
     {
-        
+        var actionMap = playerInputs.FindActionMap(actionMapStr);
+
+        movementAction = actionMap.FindAction(movementActionStr);
+        movementAction.performed += OnMovementChanged;
+        movementAction.canceled += OnMovementChanged;
+        movementAction.Enable();
+
+        firingAction = actionMap.FindAction(firingActionStr);
+        firingAction.performed += OnFiringChanged;
+        firingAction.canceled += OnFiringChanged;
+        firingAction.Enable();
+
+        aimingAction = actionMap.FindAction(aimingActionStr);
+        aimingAction.performed += OnAimingChanged;
+        aimingAction.canceled += OnAimingChanged;
+        aimingAction.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMovementChanged(InputAction.CallbackContext context)
     {
-        
+        movementValue = context.ReadValue<Vector2>();
+    }
+
+    private void OnFiringChanged(InputAction.CallbackContext context)
+    {
+        firingValue = context.ReadValue<bool>();
+    }
+
+    private void OnAimingChanged(InputAction.CallbackContext context)
+    {
+        aimingValue = context.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetMovement()
+    {
+        return movementValue;
+    }
+
+    public bool GetFiring()
+    {
+        return firingValue;
+    }
+
+    public Vector2 GetAim()
+    {
+        return aimingValue;
     }
 }
