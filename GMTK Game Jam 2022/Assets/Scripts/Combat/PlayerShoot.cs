ef58c760e9b5a5f88b3bfd1e8ddcaf5bcfb98ecd
SebaldDM13 <64290663+SebaldDM13@users.noreply.github.com>
2022-07-15 23:00:30 -0500

    PlayerShoot update

--- GMTK Game Jam 2022/Assets/Scripts/Combat/PlayerShoot.cs
+++ GMTK Game Jam 2022/Assets/Scripts/Combat/PlayerShoot.cs
@@ -4,8 +4,19 @@ public class PlayerShoot : MonoBehaviour
 {
     [SerializeField] private PlayerInputs inputs;
     [SerializeField] private GameObject projectile;
+    [SerializeField] private float velocity = 4f;
+    [SerializeField] private float firingDelay = 0.5f;
+    private float firingDelayRemaining = 0f;
 
     void Update()
-    {   
+    {
+        firingDelayRemaining = Mathf.Max(0f, firingDelayRemaining - Time.deltaTime);
+
+        if(inputs.GetFiring() > 0f && firingDelayRemaining == 0f)
+        {
+            GameObject obj = Instantiate(projectile, transform.position, transform.rotation);
+            obj.GetComponent<Rigidbody>().velocity = new Vector3(velocity, 0f, 0f);
+            firingDelayRemaining += firingDelay;
+        }
     }
 }
