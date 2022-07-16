c1fa8fd2fe953d4442f964eec579f52d316fd0c1
Lucas Ackman <75407333+Sacred-Sticks@users.noreply.github.com>
2022-07-15 19:07:10 -0500

    Added Lifetime to bullet

--- GMTK Game Jam 2022/Assets/Scripts/Bullet.cs
+++ GMTK Game Jam 2022/Assets/Scripts/Bullet.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private float lifetime;
     [SerializeField] private Vector3 velocity;
+    [SerializeField] private string targetStr;
 
     private Rigidbody body;
+    private Transform target;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody>();
+        target = GameObject.Find(targetStr).transform;
     }
 
     private void Start()
     {
-        body.velocity = velocity;
+        if (target.position.x > transform.position.x) 
+            body.velocity = velocity;
+        else body.velocity = -velocity;
+
+        StartCoroutine(DeathCycle());
+    }
+
+    private IEnumerator DeathCycle()
+    {
+        yield return new WaitForSeconds(lifetime);
+        Destroy(this.gameObject);
     }
 }
